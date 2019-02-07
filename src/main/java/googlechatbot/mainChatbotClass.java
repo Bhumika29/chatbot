5b4508a9cf5f09fd64af2844ce07bba4674bde75
HimaniDesai <de99hi@gmail.com>
2019-02-07 16:10:01 +0530

    Add files via upload

---
+++ src/main/java/googlechatbot/mainChatbotClass.java
@@ -0,0 +1,74 @@
+package googlechatbot;
+
+import java.io.File;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+ 
+public class mainChatbotClass{
+    private static final boolean TRACE_MODE = false;
+    static String botName = "super";
+    static String textLine = "";
+    public static void main(String[] args) {
+        try {
+ 
+            String resourcesPath = getResourcesPath();
+            System.out.println(resourcesPath);
+            MagicBooleans.trace_mode = TRACE_MODE;
+            Bot bot = new Bot("super", resourcesPath);
+            Chat chatSession = new Chat(bot);
+            bot.brain.nodeStats();
+            String res[][]=new String[5][2];
+ 
+            while(true) {
+            	
+                System.out.print("Human : ");
+                textLine = IOUtils.readInputTextLine();
+                if ((textLine == null) || (textLine.length() < 1))
+                    textLine = MagicStrings.null_input;
+                if (textLine.equals("q")) {
+                    System.exit(0);
+                } else if (textLine.equals("wq")) {
+                    bot.writeQuit();
+                    System.exit(0);
+                } else {
+                    String request = textLine;
+                    if (MagicBooleans.trace_mode)
+                        System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                    String response = chatSession.multisentenceRespond(request);
+                    while (response.contains("&lt;"))
+                        response = response.replace("&lt;", "<");
+                    while (response.contains("&gt;"))
+                        response = response.replace("&gt;", ">");
+                    if(response==null)
+                    {
+                    	res=GoogleSearch.doSearch(textLine);
+                        for(int i=0;i<5;i++)
+                        {
+                        	System.out.println("Text is:  "+res[i][0]+"\n URL is:  "+res[i][1]);
+                        }
+                    }
+                    System.out.println("Robot : " + response);
+                    System.out.println("This block");
+                }
+                
+                
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+            
+        }
+    }
+ 
+    private static String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+        return resourcesPath;
+    }
+}
